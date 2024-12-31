9ef65b9c21e8ec15e23bab01cafabc925cb18c90
minhcanh230103@gmail.com <minhcanh230103@gmail.com>
2024-12-31 11:10:08 +0700

    Project

---
+++ src/layout/components/BotRocker.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="overlay toggle-icon"></div>
+	<footer class="page-footer">
+		<p class="mb-0">technology © 2024.</p>
+	</footer>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
